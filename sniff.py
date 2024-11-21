0a303ba4e304d876f0ff1a97582c99eee8d0a507
0xFFSierra <0xffdaemon@proton.me>
2024-11-21 12:48:42 -0600

    Delete sniff.py

--- sniff.py
+++
@@ -1,7 +0,0 @@
-from scapy.all import sniff, Dot11
-
-def packet_handler(packet):
-    if packet.haslayer(Dot11):
-        print(f"Packet: {packet.summary()}")
-
-sniff(iface="wlp2s0mon", prn=packet_handler)
